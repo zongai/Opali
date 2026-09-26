1e49cd7c95a68bf6cb5d1f7ec0dc63375198bed6
Opaline CI <ci@opaline.local>
2026-09-26 04:33:12 +0000

    fix(ios): PlayerSheetKind, FullscreenSnapshot, PlaybackContext, ThemeManager.background

--- ios/Opaline/Features/Player/WatchViewController+Stubs.swift
+++ ios/Opaline/Features/Player/WatchViewController+Stubs.swift
@@ -1,11 +1,28 @@
 import AVFoundation
 import UIKit
 
+// MARK: - Missing types
+
+enum PlayerSheetKind {
+    case comments
+    case description
+    case quality
+    case captions
+    case settings
+}
+
+struct FullscreenSnapshot {
+    var bounds: CGRect = .zero
+    var transform: CGAffineTransform = .identity
+}
+
+// MARK: - WatchViewController stubs
+
 extension WatchViewController {
     func setupLayout() {}
     func setupNavigationBar() { title = initialVideo.title }
     func applyTheme() {
-        view.backgroundColor = ThemeManager.shared.current.background
+        view.backgroundColor = ThemeManager.shared.background
     }
     func applyResumePolicy(for video: Video) {}
     func loadInitialState() {}
@@ -15,17 +32,24 @@ extension WatchViewController {
     func updateLayoutForSize() {}
     func adjustForFloatingNavBar() {}
     func stopObservingPlayerItem(_ item: AVPlayerItem) {}
+    func exitFullscreenIfNeeded() {}
+    func keepOpeningOrientation() {}
+
+    // PlaybackContext
+    func attachPrepared(_ prepared: PreparedPlayback, resumeAt: CMTime?) {}
+    func updateStatusLabel(_ text: String) {}
+    func showPlaybackError(_ message: String) {}
+    func startObservingPlayerItem(_ item: AVPlayerItem) {}
+    func setCaptionTracks(_ tracks: [SubtitleTrack]) {}
 }
+
+extension WatchViewController: PlaybackContext {}
 
 extension VideoPlayerView {
     func detach() {
         player?.pause()
         player = nil
     }
-}
-
-extension WatchViewController {
-    func exitFullscreenIfNeeded() {}
 }
 
 func refreshSupportedOrientations() {}
